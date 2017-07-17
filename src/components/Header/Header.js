78d185742f19214b6bf6fce7491a0ee16bb01e86
李凡 <fan.li@acorn-net.com>
2017-07-17 18:00:13 +0800

    添加 auth 路由

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,9 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
+export const Header = ({title}) => (
+  <div className="header">
+    {title}
   </div>
 )
 
